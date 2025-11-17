483327f85a0c98e5a2afb97b3392b16919049ddf
Mikhail Romantsov <miromantsov@yandex.ru>
2025-11-17 16:57:15 +0300

    Repaired a piece of unresolved merge conflict.

--- lambdawars/python/core/buildings/base.py
+++ lambdawars/python/core/buildings/base.py
@@ -307,16 +307,7 @@ priobuildinglist = CreateUnitList()
 
 if isclient:
     class UnitProgressBarScreen(UnitBarScreen):
-<<<<<<< Updated upstream
-<<<<<<< HEAD
         """Draws the building construction progress bar."""
-=======
-        """ Draws the unit health bar. """
-
->>>>>>> origin/master
-=======
-        """Draws the building construction progress bar."""
->>>>>>> Stashed changes
         def __init__(self, unit):
             """Initialize the progress bar for a building.
             
